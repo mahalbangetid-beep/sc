d190133f08ca2121cc39383870bc7f8cf029d346
mahalbangetid-beep <mahalbangetid@gmail.com>
2025-08-20 10:29:57 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,10 +6,10 @@ if _G.__CHESS_AI_LOADED__FULL then
 end
 _G.__CHESS_AI_LOADED__FULL = true
 
-modules.config = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/cb/refs/heads/main/config.lua"))()
-modules.state = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/cb/refs/heads/main/state.lua"))()
-modules.ai = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/cb/refs/heads/main/ai.lua"))()
-modules.gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/cb/refs/heads/main/gui.lua"))()
-modules.chess_engine = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/cb/refs/heads/main/chess_engine.lua"))()
+modules.config = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/sc/refs/heads/main/config.lua"))()
+modules.state = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/sc/refs/heads/main/state.lua"))()
+modules.ai = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/sc/refs/heads/main/ai.lua"))()
+modules.gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/sc/refs/heads/main/gui.lua"))()
+modules.chess_engine = loadstring(game:HttpGet("https://raw.githubusercontent.com/mahalbangetid-beep/sc/refs/heads/main/chess_engine.lua"))()
 
 modules.gui.init(modules)
